15a16f4e6c73170b8e12e829521905c1e000b869
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-07-03 23:22:35 -0300

    Estou morrendo de sono.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,35 @@ import { crews } from "./components/Arrays";
 function App() {
   const [collaborators, setCollaborators] = useState([])
 
+  const [crews, setCrews] = useState([{
+    name: "Programação",
+    color: "#D9F7E9"
+  },
+  {
+    name: "Front-End",
+    color: "#E8F8FF"
+  },
+  {
+    name: "Data Science",
+    color: "#F0F8E2"
+  },
+  {
+    name: "DevOps",
+    color: "#FDE7E8"
+  },
+  {
+    name: "UX e Design",
+    color: "#FAE9F5"
+  },
+  {
+    name: "Mobile",
+    color: "#FFF5D9"
+  },
+  {
+    name: "Inovação e Gestão",
+    color: "#FFEEDF"
+  }])
+
   const onNewCollaboratorAdded = (collaborator) => {
     console.log(collaborator)
     setCollaborators([...collaborators, collaborator])
@@ -15,6 +44,16 @@ function App() {
   const deleteCollaborator = (id) => {
     setCollaborators(collaborators.filter(collaborator => collaborator.id !== id))
   }
+
+  function changeCrewColor(color, name) {
+    setCrews(crews.map(crew => {
+      if (crew.name === name) {
+        crew.color = color
+      }
+      return crew
+    }))
+  }
+
   return (
     <div className="App">
       <Banner />
@@ -23,10 +62,11 @@ function App() {
         onRegisteredCollaborator={collaborator => onNewCollaboratorAdded(collaborator)}
       />
       {crews.map(crews => <Crew
+        changeColor={changeCrewColor}
         key={crews.name}
         title={crews.name}
         primaryColor={crews.primaryColor}
-        secondaryColor={crews.secondaryColor}
+        secondaryColor={crews.color}
         collaborators={collaborators.filter(collaborator => collaborator.crew === crews.name)}
         onDelete={deleteCollaborator}
       />)}
@@ -35,3 +75,5 @@ function App() {
 }
 
 export default App;
+
+// PAREI EM 05/03
--- src/components/Crew/index.js
+++ src/components/Crew/index.js
@@ -1,12 +1,25 @@
 import { Collaborator } from "../Collaborator"
 import "./Crew.css"
 
-export const Crew = ({ title, primaryColor, secondaryColor, collaborators, onDelete, id }) => {
-  // console.log(collaborators)
+export const Crew = ({
+  title,
+  primaryColor,
+  color,
+  collaborators,
+  onDelete,
+  id,
+  changeColor,
+}) => {
   return (
     <>
       {collaborators.length > 0 &&
-        <section className="crew" style={{ backgroundColor: secondaryColor }}>
+        <section className="crew" style={{ backgroundColor: color }}>
+          <input
+            onChange={(event) => changeColor(event.target.value, title)}
+            value={color}
+            type="color"
+            className="input-color"
+          />
           <h3 style={{ borderColor: primaryColor }}>{title}</h3>
           <div className="collaborators">
             {collaborators.map((collaborator) => {
